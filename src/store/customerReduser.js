f31992f4a503df44e07825ca64082dab1cde4e92
Irina Shorokhova <irinaais90@gmail.com>
2024-12-09 19:16:13 +0300

    fix: fetching users

--- src/store/customerReduser.js
+++ src/store/customerReduser.js
@@ -8,9 +8,10 @@ const initialValue ={
 export const customerReducer = (state = initialValue, action) => {
     switch (action.type) {
         case SET_USERS:
-            return {...state, users: [...state.users, ...action.payload]};       
+            return {...state, users: [...state.users, ...action.payload]};
+        default:
+            return state;
     }
-    return state
 };
 export const setUsers = (payload) => ({ type: SET_USERS, payload });
 export const fetchUsers = () => ({ type: FETCH_USERS});
